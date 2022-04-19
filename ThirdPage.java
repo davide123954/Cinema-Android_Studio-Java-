1ca1b6318c759404bc760c724499a50da99cb087
Davide <66334191+davide123954@users.noreply.github.com>
2022-04-19 11:43:55 +0300

    Create ThirdPage.java
    In this page is just for showing the cinema area

---
+++ ThirdPage.java
@@ -0,0 +1,14 @@
+package com.example.projectigor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThreeForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_three_form);
+    }
+}
